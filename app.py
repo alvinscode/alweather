20eb78cc86cd449fc1010e5d84e2c64ef9cfd7b1
Alvin Li <aflip24@gmail.com>
2024-02-16 10:46:17 -0600

    change host

--- app.py
+++ app.py
@@ -17,4 +17,4 @@ def styles():
     return send_from_directory(os.path.dirname(__file__), 'styles.css')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
